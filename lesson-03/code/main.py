00e3f57d95ed4141f996d7e18709627dcaa76bc1
Michael Saunby <mike@saunby.net>
2021-01-02 16:51:49 +0000

    .

--- lesson-03/code/main.py
+++ lesson-03/code/main.py
@@ -2,6 +2,10 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 
+class MyTextInput(Widget):
+    def print_text(self,text):
+        print(text)
+
 class Example(Widget):
 
    def update(self, dt):
